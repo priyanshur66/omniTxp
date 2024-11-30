6d2e0d62b9f7210b92c7620da12a4bff11ecd15e
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-12-01 02:02:43 +0530

    test twlwgram web app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { OktoProvider, BuildType } from 'okto-sdk-react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage';
 import HomePage from './HomePage';
+import WebApp from "@twa-dev/sdk";
 
 const OKTO_CLIENT_API_KEY = process.env.REACT_APP_OKTO_CLIENT_API_KEY;
 
@@ -14,6 +15,14 @@ function App() {
     console.log("setting auth token to null");
     setAuthToken(null);
   };
+  useEffect(() => {
+    console.log("test twa")
+    console.log(WebApp.initDataUnsafe.user)
+    if (WebApp.initDataUnsafe.user) {
+      console.log("test twa2")
+      console.log(WebApp.initDataUnsafe.user);
+    }
+  });
 
   return (
     <Router>
