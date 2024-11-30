3f0ab05c02a54805adeb6f881c40856f0bf02dde
priyanshur66 <priyanshuranjan0001@gmail.com>
2024-12-01 01:54:36 +0530

    transactions working

--- src/App.js
+++ src/App.js
@@ -2,25 +2,45 @@ import React, { useState } from 'react';
 import { OktoProvider, BuildType } from 'okto-sdk-react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './LoginPage';
-
-
+import HomePage from './HomePage';
 
 const OKTO_CLIENT_API_KEY = process.env.REACT_APP_OKTO_CLIENT_API_KEY;
+
 function App() {
- console.log('App component rendered');
- const [authToken, setAuthToken] = useState(null);
- const handleLogout = () => {
-    console.log("setting auth token to null")
-    setAuthToken(null); // Clear the authToken
+  console.log('App component rendered');
+  const [authToken, setAuthToken] = useState(null);
+
+  const handleLogout = () => {
+    console.log("setting auth token to null");
+    setAuthToken(null);
   };
- return (
-   <Router>
-     <OktoProvider apiKey={OKTO_CLIENT_API_KEY} buildType={BuildType.SANDBOX}>
-       <Routes>
-         <Route path="/" element={<LoginPage setAuthToken={setAuthToken} authToken={authToken} handleLogout={handleLogout}/>} />
-       </Routes>
-     </OktoProvider>
-   </Router>
- );
+
+  return (
+    <Router>
+      <OktoProvider apiKey={OKTO_CLIENT_API_KEY} buildType={BuildType.SANDBOX}>
+        <Routes>
+          <Route 
+            path="/" 
+            element={
+              <LoginPage 
+                setAuthToken={setAuthToken} 
+                authToken={authToken} 
+                handleLogout={handleLogout}
+              />
+            } 
+          />
+          <Route 
+            path="/home" 
+            element={
+              authToken ? 
+                <HomePage authToken={authToken} handleLogout={handleLogout}/> : 
+                <Navigate to="/" />
+            } 
+          />
+        </Routes>
+      </OktoProvider>
+    </Router>
+  );
 }
+
 export default App;
